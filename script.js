d9f04bf5d64b26a9d6f6c805e5c536441e6d7d60
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-01 00:08:42 +0300

    feat: Add a function that takes the user's input
    A function that prompts the user to enter there choice between rock, paper or scissors (the prompt is not case sensitive). The function also converts the choice into one of  three integers, either 1,2 or 3.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function computerSelection() {
-    return Math.floor(Math.random() * 3) + 1;   // A function that returns an integer between 1 & 3
+    return Math.floor(Math.random() * 3) + 1;                                                                   // A function that returns an integer between 1 & 3
 }
 
-console.log(computerSelection());
+
+
+function playerSelection() {
+    let userInput = prompt(`Enter your warrior of choice: "rock", "paper" or "scissors"`).toLocaleLowerCase();  // The user inputs there choice. Choice is not case sensitive
+
+    if (userInput == 'paper') {                                                                                 // User's choice is converted to an integer representing the given input choice
+        return 1;
+    } else if (userInput == 'scissors') {                                                                       // Each integer between 1 & 3 represents one of the three input choices:
+        return 2;                                                                                               // 1 is paper
+    } else if (userInput == 'rock') {                                                                           // 2 is scissors                                                
+        return 3;                                                                                               // 3 is rock
+    } else {
+        alert('Wrong input');
+    }
+}
+
+
+
