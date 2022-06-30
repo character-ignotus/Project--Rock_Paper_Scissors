2a13bc644c40e27f8d89a167207d5312b085de6e
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-01 01:03:27 +0300

    feat: Add a function that evaluates both choices
    A function that takes both the user's choice and the computer's choice, evaluates them & decides wich of the combination functions to activate. The combination functions are used to determine a winner, or if there is a tie.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function computerSelection() {
 }
 
 
-
 function playerSelection() {
     let userInput = prompt(`Enter your warrior of choice: "rock", "paper" or "scissors"`).toLocaleLowerCase();  // The user inputs there choice. Choice is not case sensitive
 
@@ -15,8 +14,27 @@ function playerSelection() {
         return 3;                                                                                               // 3 is rock
     } else {
         alert('Wrong input');
+        return
     }
 }
+
+function playRound(player = playerSelection(), computer = computerSelection()) {                                // A function that takes both the user & the computer's choice & determines wich Combination function to initialize
+                                  
+    if ((player == 1 || player == 3) && (computer == 1 || computer == 3)) {                                     // The Combination functions are used to determine who wins & who looses or if its a tie
+        Combination1(player, computer);
+    } else if ((player == 1 || player == 2) && (computer == 1 || computer == 2)) {                              // There are three possible combinations:
+        Combination2(player, computer);
+    } else {                                                                                                    // Combination1: rock-rock, paper-paper, paper-rock or rock-paper choice combination
+        Combination3(player, computer);                                                                         // Combination2: paper-paper, scissors-scissors, paper-scissors or scissors-paper choice combination
+    }  
+                                                                                                                // Combination3: scissors-scissors, rock-rock, scissors-rock or rock-scissors choice combination
+}
+
+
+
+
+
+
 
 
 
