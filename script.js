dac2e30923492a48efdb4564e1d9e872c4b6bcc9
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-02 21:59:27 +0300

    feat: Remove draw as an outcome
    With this commit the draw outcome from the single rounds as well as from the final outcome is removed. Now the game's outcome is based on the concept of 'Best three out of five', & every round must declare a winner.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function playerSelection() {
 }
 
 function playRound(player = playerSelection(), computer = computerSelection()) {                                // A function that takes both the user & the computer's choice & determines wich Combination function to initialize                            
-    if ((player == 1 || player == 3) && (computer == 1 || computer == 3)) {                                     // The Combination functions are used to determine who wins & who looses or if its a tie
+    if ((player == 1 || player == 3) && (computer == 1 || computer == 3)) {                                     // The Combination functions are used to determine who wins the round
         Combination1(player, computer);
     } else if ((player == 1 || player == 2) && (computer == 1 || computer == 2)) {                              // There are three possible combinations:
         Combination2(player, computer);
@@ -35,26 +35,22 @@ let program = 0;                                                                
 
 function Combination1(userChoice, programChoice) {                                                              // Function for Combination1
 
-    if (userChoice == 1 && programChoice == 1) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices                                   
+    if (userChoice == 1 && programChoice == 3) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices                                   
         uS = 'paper';
-        pS = 'paper';
-    } else if (userChoice == 3 && programChoice == 3) {
-        uS = 'rock';
         pS = 'rock';
-    } else if (userChoice == 1) {
-        uS = 'paper';
-        pS = 'rock'
-    } else {
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else if (userChoice == 3 && programChoice == 1) {
         uS = 'rock';
         pS = 'paper';
-    }
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else {}
 
-    console.log(`USER HAS CHOSEN: ${uS}`);                                                                      // The user & the computer's choices are printed out
-    console.log(`PROGRAM HAS CHOSEN: ${pS}`);
 
-    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round or if there is a tie for Combination1
-        console.log('This round is a tie');
-        console.log('');
+    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round
+        alert('We have a tie this round! Play again');                                                          // If both the user & the computer's choices are the same then the round is played again until a winner is determined
+        playRound();
     } else if (!(userChoice > programChoice)) {
         console.log('User wins this round! Paper beats rock!');
         console.log('');
@@ -68,26 +64,22 @@ function Combination1(userChoice, programChoice) {                              
 
 function Combination2(userChoice, programChoice) {                                                              // Function for Combination2
 
-    if (userChoice == 1 && programChoice == 1) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices                                   
+    if (userChoice == 1 && programChoice == 2) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices                                   
         uS = 'paper';
-        pS = 'paper';
-    } else if (userChoice == 2 && programChoice == 2) {
-        uS = 'scissors';
         pS = 'scissors';
-    } else if (userChoice == 1) {
-        uS = 'paper';
-        pS = 'scissors'
-    } else {
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out                                                                 
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else if (userChoice == 2 && programChoice == 1) {
         uS = 'scissors';
         pS = 'paper';
-    }
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out                                                                 
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else {}
+    
 
-    console.log(`USER HAS CHOSEN: ${uS}`);                                                                      // The user & the computer's choices are printed out                                                                 
-    console.log(`PROGRAM HAS CHOSEN: ${pS}`);
-
-    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round or if there is a tie for Combination2
-        console.log('This round is a tie');
-        console.log('');
+    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round
+        alert('We have a tie this round! Play again');                                                          // If both the user & the computer's choices are the same then the round is played again until a winner is determined
+        playRound();
     } else if (userChoice > programChoice) {
         console.log('User wins this round! Scissors beat paper!');
         console.log('');
@@ -101,26 +93,22 @@ function Combination2(userChoice, programChoice) {                              
 
 function Combination3(userChoice, programChoice) {                                                              // Function for Combination3
 
-    if (userChoice == 2 && programChoice == 2) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices
+    if (userChoice == 2 && programChoice == 3) {                                                                // An If Else statement is initialized to determine both the user & the computer's choices
         uS = 'scissors';
-        pS = 'scissors';
-    } else if (userChoice == 3 && programChoice == 3) {
-        uS = 'rock';
         pS = 'rock';
-    } else if (userChoice == 2) {
-        uS = 'scissors';
-        pS = 'rock'
-    } else {
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else if (userChoice == 3 && programChoice == 2) {
         uS = 'rock';
         pS = 'scissors';
-    }
+        console.log(`USER HAS CHOSEN: ${uS}`);                                                                  // The user & the computer's choices are printed out
+        console.log(`PROGRAM HAS CHOSEN: ${pS}`);
+    } else {}
+        
 
-    console.log(`USER HAS CHOSEN: ${uS}`);                                                                      // The user & the computer's choices are printed out
-    console.log(`PROGRAM HAS CHOSEN: ${pS}`);
-
-    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round or if there is a tie for Combination3
-        console.log('This round is a tie');
-        console.log('');
+    if (userChoice == programChoice) {                                                                          // An If Else statement to evaluate the winner of the round
+        alert('We have a tie this round! Play again');                                                          // If both the user & the computer's choices are the same then the round is played again until a winner is determined
+        playRound();
     } else if (userChoice > programChoice) {
         console.log('User wins this round! Rock beats scissors!');
         console.log('');
@@ -133,26 +121,24 @@ function Combination3(userChoice, programChoice) {                              
 }
 
 function game() {                                                                                               // The game function is used to play five rounds of the game
-    for(i=0; i<5; ++i) {                                                                                        // A for loop goes over the playround function (wich is used to play a single round) five times
+    for(i=0; i<5; ++i) {                                                                                        // A for loop goes over the playRound function (wich is used to play a single round) five times
 
         playRound();
 
-        if ((user == 3) || (user == 2 && program == 0 && i>2)) {                                                // An If Else statement in the loop to check wheter a result should be declared after five or after four rounds.
+        if (user == 3) {                                                                                        // An If Else statement in the loop to check wheter a result should be declared after five rounds or earlier
             break;                                                                                              // The verification for that is based on the outcomes of the single rounds
-        } else if ((program == 3) || (program == 2 && user == 0 && i>2)) {
+        } else if (program == 3) {
             break;
         } else {
             continue;
         }
     }
 
-    if (user > program) {                                                                                       // An If Else statement to check & declare the final winner or tie if there is one
+    if (user > program) {                                                                                       // An If Else statement to check & declare the final winner 
         console.log('User wins the game!');
     } else if (user < program) {
         console.log('Computer wins the game!');
-    } else {
-        console.log('We have a tie');
-    }
+    } else {}
 }
 
 game();
