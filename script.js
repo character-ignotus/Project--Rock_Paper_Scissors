dc1df3c22c15190df2ae45b7a141880b951212e5
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-21 18:25:08 +0300

    Display score, current round & round winner

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const userScore = document.getElementById('user-score');
 const computerChoice = document.querySelector('.sub.one div div');
 const playerChoice = document.querySelector('.sub.five div div');
 const outcome = document.querySelector('.sub.three');
+const subContainer2 = document.querySelector('.subContainer.two')
 
+
+const roundOutcome = document.createElement('div');
 
 paperBtn.addEventListener('click', () => {
     let playerSelection = 1;
@@ -43,7 +46,8 @@ function playRound(player, computer) {
 
 
 let user = 0;                                                                                                   // Variable for storing the user's score
-let program = 0;                                                                                                // Variable for storing the computer's score
+let program = 0; 
+let round = 0;                                                                                               // Variable for storing the computer's score
 
                                                                                                                 
 function Combination1(userChoice, programChoice) {                                                              //FUNCTION FOR COMBINATION1
@@ -61,21 +65,32 @@ function Combination1(userChoice, programChoice) {                              
     } else {}
 
 
-    if (userChoice == programChoice) {                                                                              // 2. An If Else statement to evaluate the winner of the round
-        alert('We have a tie this round! Play again');                                                                  // If both the user & the computer's choices are the same then the round is played again until a winner is determined
-        playRound();
+    if (userChoice == programChoice) {                                                                             
+        alert('We have a tie this round! Play again');                                                                  
+        roundOutcome.textContent = 'We have a tie this round! Play again';
+        outcome.appendChild(roundOutcome);
     } else if (!(userChoice > programChoice)) {
-        console.log('User wins this round! Paper beats rock!');
-        ++user;                                                                                                         // If the user wins, there variable is incremented by one
+        roundOutcome.textContent = 'User wins this round! Paper beats rock!';
+        outcome.appendChild(roundOutcome);
+        user++;
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                         
     } else {
-        console.log('Program wins this round! Paper beats rock!');
-        ++program;                                                                                                      // If the program wins, there variable is incremented by one
+        roundOutcome.textContent = 'Program wins this round! Paper beats rock!';
+        outcome.appendChild(roundOutcome);
+        program++; 
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                      
     }
 }
 
-function Combination2(userChoice, programChoice) {                                                              //FUNCTION FOR COMBINATION2
+function Combination2(userChoice, programChoice) {                                                             
 
-    if (userChoice == 1 && programChoice == 2) {                                                                    // 1. An If Else statement is initialized to determine both the user & the computer's choices                                   
+    if (userChoice == 1 && programChoice == 2) {                                                                                               
         uS = 'paper';
         pS = 'scissors';
         playerChoice.textContent = `${uS}`;
@@ -88,21 +103,32 @@ function Combination2(userChoice, programChoice) {                              
     } else {}
     
 
-    if (userChoice == programChoice) {                                                                              // 2. An If Else statement to evaluate the winner of the round
-        alert('We have a tie this round! Play again');                                                                  // If both the user & the computer's choices are the same then the round is played again until a winner is determined
-        playRound();
+    if (userChoice == programChoice) {                                                                              
+        alert('We have a tie this round! Play again');                                                                  
+        roundOutcome.textContent = 'We have a tie this round! Play again';
+        outcome.appendChild(roundOutcome);
     } else if (userChoice > programChoice) {
-        console.log('User wins this round! Scissors beat paper!');
-        ++user;                                                                                                         // If the user wins, there variable is incremented by one
+        roundOutcome.textContent = 'User wins this round! Scissors cuts paper!';
+        outcome.appendChild(roundOutcome);
+        user++;
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                        
     } else {
-        console.log('Program wins this round! Scissors beat paper!');
-        ++program;                                                                                                      // If the program wins, there variable is incremented by one
+        roundOutcome.textContent = 'Program wins this round! Scissors cuts paper!';
+        outcome.appendChild(roundOutcome);
+        program++;
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                      
     }
 }
 
-function Combination3(userChoice, programChoice) {                                                              //FUNCTION FOR COMBINATION3
+function Combination3(userChoice, programChoice) {                                                             
 
-    if (userChoice == 2 && programChoice == 3) {                                                                    // 1. An If Else statement is initialized to determine both the user & the computer's choices
+    if (userChoice == 2 && programChoice == 3) {                                                                    
         uS = 'scissors';
         pS = 'rock';
         playerChoice.textContent = `${uS}`;
@@ -115,28 +141,39 @@ function Combination3(userChoice, programChoice) {                              
     } else {}
         
 
-    if (userChoice == programChoice) {                                                                              // 2. An If Else statement to evaluate the winner of the round
-        alert('We have a tie this round! Play again');                                                                  // If both the user & the computer's choices are the same then the round is played again until a winner is determined
-        playRound();
+    if (userChoice == programChoice) {                                                                             
+        alert('We have a tie this round! Play again');                                                                
+        roundOutcome.textContent = 'We have a tie this round! Play again';
+        outcome.appendChild(roundOutcome);
     } else if (userChoice > programChoice) {
-        console.log('User wins this round! Rock beats scissors!');
-        ++user;                                                                                                         // If the user wins, there variable is incremented by one
+        roundOutcome.textContent = 'User wins this round! Rock beats scissors!';
+        outcome.appendChild(roundOutcome);
+        user++;
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                        
     } else {
-        console.log('Program wins this round! Rock beats scissors!');
-        ++program;                                                                                                      // If the program wins, there variable is incremented by one
+        roundOutcome.textContent = 'Program wins this round! Rock beats scissors!';
+        outcome.appendChild(roundOutcome);
+        program++;
+        round++;
+        roundCounter.textContent = `${round}`;
+        computerScore.textContent = `${program}`;
+        userScore.textContent = `${user}`;                                                                                                     
     }
 }
                                                                                                                 
-function game() {                                                                                               // THE GAME FUNCTION IS USED FOR PLAYING TEN ROUNDS OF THE GAME OR LESS DEPENDING ON THE SINGLE ROUND OUTCOME
-    for(i=1; i<11; ++i) {                                                                                           // 1. A for loop goes over the playRound function (wich is used to play a single round) a maximum of ten times 
+function game() {                                                                                               
+    for(i=1; i<11; ++i) {                                                                                          
 
         playRound();
 
-        console.log(`ROUND ${i} SCORE IS --> user: ${user} / program:${program}`);                                  // 2. Printout the user & the computer's scores for each round
+        console.log(`ROUND ${i} SCORE IS --> user: ${user} / program:${program}`);                                  
         console.log('');
 
-        if (user == 5) {                                                                                            // 3. An If Else statement in the loop to check wheter a result should be declared after ten rounds or earlier
-            break;                                                                                                      // - The verification for that is based on the outcomes of the single rounds
+        if (user == 5) {                                                                                          
+            break;                                                                                                 
         } else if (program == 5) {
             break;
         } else {
@@ -144,7 +181,7 @@ function game() {                                                               
         }
     }
 
-    if (user > program) {                                                                                           // 4. An If Else statement to check & declare the final winner 
+    if (user > program) {                                                                                           
         console.log('USER WINS THE GAME!');
         alert('You win the game! Computer has been annihilate!')
     } else if (user < program) {
