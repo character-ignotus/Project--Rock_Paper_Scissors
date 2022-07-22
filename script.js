1c681460586d180b5461767d6a72f1704834bf53
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-07-22 13:45:44 +0300

    feat: Add css to page
    This commit applies the layout structure of the page, the element colors, fonts as well as features like button scale transformations when certain mouse events are performed.

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ const rockBtn = document.querySelector('.rock.user');
 const roundCounter = document.getElementById('roundCounter');
 const computerScore = document.getElementById('computer-score');
 const userScore = document.getElementById('user-score');
-const computerChoice = document.querySelector('.sub.one div div');
-const playerChoice = document.querySelector('.sub.five div div');
+const computerChoice = document.querySelector('#computer-choice');
+const playerChoice = document.querySelector('#user-choice');
 const outcome = document.querySelector('.sub.three');
 const subContainer2 = document.querySelector('.subContainer.two');
 const playerButtons = document.querySelector('.sub.four');
 const computerButtons = document.querySelector('.sub.two');
+const subOne = document.querySelector('.sub.one');
+const subFive= document.querySelector('.sub.five');
 
-playerButtons.style.display = 'none';
-computerButtons.style.display = 'none';
+playerChoice.setAttribute('style', 'font-style: italic; color: #f3f1d2;');
+computerChoice.setAttribute('style', 'font-style: italic; color: #f3f1d2;');
+
+playerButtons.classList.add('invisible');
+computerButtons.classList.add('invisible');
 
 const roundOutcome = document.createElement('div');
 const startBtn = document.createElement('button');
 const playAgainBtn = document.createElement('button');
+
+startBtn.classList.add('Btn');
+playAgainBtn.classList.add('Btn');
 
 startBtn.textContent = 'START GAME';
 playAgainBtn.textContent = 'PLAY AGAIN';
@@ -24,28 +32,69 @@ playAgainBtn.textContent = 'PLAY AGAIN';
 outcome.appendChild(startBtn);
 
 subContainer2.insertBefore(playAgainBtn, playerButtons);
-playAgainBtn.style.display = 'none';
+playAgainBtn.classList.add('invisible');
 
 startBtn.addEventListener('click', () => {
-    playerButtons.style.display = 'block';
-    computerButtons.style.display = 'block';
+    playerButtons.classList.remove('invisible');
+    computerButtons.classList.remove('invisible');
+    playerButtons.classList.add('visible');
+    computerButtons.classList.add('visible');
     outcome.removeChild(startBtn);
 });
 
-paperBtn.addEventListener('click', () => {
+paperBtn.addEventListener('mousedown', () => {
+    paperBtn.classList.add('mouseDown');
     let playerSelection = 1;
     playRound(playerSelection, computer = computerSelection());
 });
 
-scissorsBtn.addEventListener('click', () => {
+        paperBtn.addEventListener('mouseup', () => {
+            paperBtn.classList.remove('mouseDown');
+        });
+
+        paperBtn.addEventListener('mouseenter', () => {
+            paperBtn.classList.add('mouseEnter');
+        });
+
+        paperBtn.addEventListener('mouseleave', () => {
+            paperBtn.classList.remove('mouseEnter');
+        });
+
+scissorsBtn.addEventListener('mousedown', () => {
+    scissorsBtn.classList.add('mouseDown');
     let playerSelection = 2;
     playRound(playerSelection, computer = computerSelection());
 });
 
-rockBtn.addEventListener('click', () => {
+        scissorsBtn.addEventListener('mouseup', () => {
+            scissorsBtn.classList.remove('mouseDown');
+        });
+
+        scissorsBtn.addEventListener('mouseenter', () => {
+            scissorsBtn.classList.add('mouseEnter');
+        });
+
+        scissorsBtn.addEventListener('mouseleave', () => {
+            scissorsBtn.classList.remove('mouseEnter');
+        });
+
+rockBtn.addEventListener('mousedown', () => {
+    rockBtn.classList.add('mouseDown');
     let playerSelection = 3;
     playRound(playerSelection, computer = computerSelection());
 });
+
+        rockBtn.addEventListener('mouseup', () => {
+            rockBtn.classList.remove('mouseDown');
+        });
+
+        rockBtn.addEventListener('mouseenter', () => {
+            rockBtn.classList.add('mouseEnter');
+        });
+
+        rockBtn.addEventListener('mouseleave', () => {
+            rockBtn.classList.remove('mouseEnter');
+        });
 
 
 
@@ -76,23 +125,18 @@ function Combination1(userChoice, programChoice) {
         uS = 'paper';
         pS = 'rock';
         playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        computerChoice.textContent =  `${pS}`;
     } else if (userChoice == 3 && programChoice == 1) {
         uS = 'rock';
-        pS = 'paper';
-        playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        pS = 'paper'; 
+        playerChoice.textContent =  `${uS}`;
+        computerChoice.textContent =  `${pS}`;
     } else {}
 
 
-    if (userChoice == programChoice) {                                                                             
-        alert('We have a tie this round! Play again');                                                                  
-        // roundOutcome.textContent = 'We have a tie this round! Play again';
-        // outcome.appendChild(roundOutcome);
+    if (userChoice == programChoice) {                                                                                          
         outcome.textContent = 'We have a tie this round! Play again';
     } else if (!(userChoice > programChoice)) {
-        // roundOutcome.textContent = 'User wins this round! Paper beats rock!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'User wins this round! Paper beats rock!';
         user++;
         round++;
@@ -100,8 +144,6 @@ function Combination1(userChoice, programChoice) {
         computerScore.textContent = `${program}`;
         userScore.textContent = `${user}`;                                                                                                         
     } else {
-        // roundOutcome.textContent = 'Program wins this round! Paper beats rock!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'Program wins this round! Paper beats rock!';
         program++; 
         round++;
@@ -119,23 +161,18 @@ function Combination2(userChoice, programChoice) {
         uS = 'paper';
         pS = 'scissors';
         playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        computerChoice.textContent = ` ${pS}`;
     } else if (userChoice == 2 && programChoice == 1) {
         uS = 'scissors';
         pS = 'paper';
-        playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        playerChoice.textContent = ` ${uS}`;
+        computerChoice.textContent = ` ${pS}`;
     } else {}
     
 
     if (userChoice == programChoice) {                                                                              
-        alert('We have a tie this round! Play again');                                                                  
-        // roundOutcome.textContent = 'We have a tie this round! Play again';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'We have a tie this round! Play again';
     } else if (userChoice > programChoice) {
-        // roundOutcome.textContent = 'User wins this round! Scissors cuts paper!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'User wins this round! Scissors cuts paper!';
         user++;
         round++;
@@ -143,8 +180,6 @@ function Combination2(userChoice, programChoice) {
         computerScore.textContent = `${program}`;
         userScore.textContent = `${user}`;                                                                                                        
     } else {
-        // roundOutcome.textContent = 'Program wins this round! Scissors cuts paper!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'Program wins this round! Scissors cuts paper!';
         program++;
         round++;
@@ -161,24 +196,19 @@ function Combination3(userChoice, programChoice) {
     if (userChoice == 2 && programChoice == 3) {                                                                    
         uS = 'scissors';
         pS = 'rock';
-        playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        playerChoice.textContent = ` ${uS}`;
+        computerChoice.textContent = ` ${pS}`;
     } else if (userChoice == 3 && programChoice == 2) {
         uS = 'rock';
         pS = 'scissors';
-        playerChoice.textContent = `${uS}`;
-        computerChoice.textContent = `${pS}`;
+        playerChoice.textContent = ` ${uS}`;
+        computerChoice.textContent = ` ${pS}`;
     } else {}
         
 
     if (userChoice == programChoice) {                                                                             
-        alert('We have a tie this round! Play again');                                                                
-        // roundOutcome.textContent = 'We have a tie this round! Play again';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'We have a tie this round! Play again';
     } else if (userChoice > programChoice) {
-        // roundOutcome.textContent = 'User wins this round! Rock beats scissors!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'User wins this round! Rock beats scissors!';
         user++;
         round++;
@@ -186,8 +216,6 @@ function Combination3(userChoice, programChoice) {
         computerScore.textContent = `${program}`;
         userScore.textContent = `${user}`;                                                                                                        
     } else {
-        // roundOutcome.textContent = 'Program wins this round! Rock beats scissors!';
-        // outcome.appendChild(roundOutcome);
         outcome.textContent = 'Program wins this round! Rock beats scissors!';
         program++;
         round++;
@@ -202,13 +230,18 @@ function Combination3(userChoice, programChoice) {
 
 function winnerDeclaration (currentUserScore, currentProgramScore) {
     if (currentUserScore == 5) {
-        playerButtons.style.display = 'none';
-        computerButtons.style.display = 'none';
+        playerButtons.classList.remove('visible');
+        computerButtons.classList.remove('visible');
+        playerButtons.classList.add('invisible');
+        computerButtons.classList.add('invisible');
+        outcome.setAttribute('id', 'outcomeSize');
         outcome.textContent = 'You win the game! Computer has been annihilated!';
-        playAgainBtn.style.display = 'block';
+        playAgainBtn.classList.remove('invisible');
+        playAgainBtn.classList.add('visible');
 
         playAgainBtn.addEventListener('click', () => {
             outcome.textContent = '';
+            outcome.removeAttribute('id', 'outcomeSize');
 
             playerChoice.textContent = '';
             computerChoice.textContent = '';
@@ -220,17 +253,27 @@ function winnerDeclaration (currentUserScore, currentProgramScore) {
             program = 0;
             round = 0;
 
-            playerButtons.style.display = 'block';
-            playAgainBtn.style.display = 'none'
+            playAgainBtn.classList.remove('visible')
+            playAgainBtn.classList.add('invisible');
+            outcome.classList.remove('outcomeSize');
+            playerButtons.classList.remove('invisible');
+            computerButtons.classList.remove('invisible');
+            playerButtons.classList.add('visible');
+            computerButtons.classList.add('visible');
         });
     } else if (currentProgramScore == 5) {
-        computerButtons.style.display = 'none';
-        playerButtons.style.display = 'none';
+        playerButtons.classList.remove('visible');
+        computerButtons.classList.remove('visible');
+        playerButtons.classList.add('invisible');
+        computerButtons.classList.add('invisible'); 
+        outcome.setAttribute('id', 'outcomeSize');      
         outcome.textContent = 'You lose! Computer has annihilated you!';
-        playAgainBtn.style.display = 'block';
+        playAgainBtn.classList.remove('invisible');
+        playAgainBtn.classList.add('visible');
 
         playAgainBtn.addEventListener('click', () => {
             outcome.textContent = '';
+            outcome.removeAttribute('id', 'outcomeSize');
 
             playerChoice.textContent = '';
             computerChoice.textContent = '';
@@ -242,8 +285,13 @@ function winnerDeclaration (currentUserScore, currentProgramScore) {
             program = 0;
             round = 0;
 
-            playerButtons.style.display = 'block';
-            playAgainBtn.style.display = 'none'
+            playAgainBtn.classList.remove('visible')
+            playAgainBtn.classList.add('invisible');
+
+            playerButtons.classList.remove('invisible');
+            computerButtons.classList.remove('invisible');
+            playerButtons.classList.add('visible');
+            computerButtons.classList.add('visible');
         });
     }
 }
